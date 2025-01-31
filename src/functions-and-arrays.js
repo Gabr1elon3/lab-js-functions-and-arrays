a7069bd1b3ffe41bf8d812d2cbbe4300b9b1a298
Gabriele Capezzuto <gabricap7@gmail.com>
2025-01-31 20:50:25 +0100

    new

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(n1,n2) {
+    if(n1>n2){
+        return n1
+    } else if (n2>n1){
+        return n2
+    }else {
+        return n1,n2
+    }
+}
 
 
 
@@ -7,15 +15,31 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
+function findLongestWord(words) {
+    let longestWord = null; // Initialize longestWord to null
+  
+    for (let i = 0; i < words.length; i++) {
+      if (!longestWord || words[i].length > longestWord.length) {
+        longestWord = words[i]; // Update the longest word if a longer word is found
+      }
+    }
+  
+    return longestWord;
+  }
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  }
+  
 
 
 
@@ -23,7 +47,16 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return 0;
+      }
+    let average=0;
+    for (let i = 0; i < numbers2.length; i++) {
+        average += numbers2[i];
+    } 
+    return average/numbers2.length
+}
 
 
 
@@ -31,4 +64,14 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+
+function doesWordExist(arr, wrdsearch) {
+    if (arr.length === 0) {
+      return null; // Return null for an empty array
+    } else if (arr.indexOf(wrdsearch) !== -1) {
+      return true; // Word exists in the array
+    } else {
+      return false; // Word doesn't exist in the array
+    }
+  }
+  
